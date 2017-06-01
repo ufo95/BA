c63cd95899092cae507c136768b6735277259a1e
ufo95 <ufo95@gmx.de>
2017-06-01 17:46:23 +0200

    [Implementation] Wrote the recover_address function The Callback, which gives me the return value of allocation syscalls, so I know the address of memory requested by the program. The Callback is unstable, need to fix the segfault, tomorrow!

--- src/plugins/packeranalyser/packeranalyser.cpp
+++ src/plugins/packeranalyser/packeranalyser.cpp
@@ -6,9 +6,98 @@
 #include "packeranalyser.h"
 #include "../plugins.h"
 #include <libdrakvuf/libdrakvuf.h>
+//TODO: Refactor code so that only one syscall argument read function is needed
 
-void recover_address(drakvuf_t drakvuf, drakvuf_trap_info_t *info){
+
+static event_response_t recover_address_cb(drakvuf_t drakvuf, drakvuf_trap_info_t *info) {
+    //TODO: check return value in eax
     //packeranalyser *p = (packeranalyser*)info->trap->data;
+    //TODO: Fixme: Gets segfaulted!!!
+
+    printf("!!!!!!!!!recover_address_cb!!!!!!!!!!!!\n");
+
+    int number_of_args = 0, index_address = 0, index_protect = 0;
+    vmi_instance_t vmi = drakvuf_lock_and_get_vmi(drakvuf);
+    uint8_t reg_size = vmi_get_address_width(vmi);
+    size_t size = 0;
+    unsigned char* buf = NULL; // pointer to buffer to hold argument values
+
+    access_context_t ctx;
+    ctx.translate_mechanism = VMI_TM_PROCESS_DTB;
+    ctx.dtb = info->regs->cr3;
+
+    size = reg_size * number_of_args;
+
+    buf  = (unsigned char *)g_malloc(sizeof(char)*size);
+
+    uint32_t *buf32 = (uint32_t *)buf;
+
+
+
+
+    if (!strcmp(info->trap->name, "NtAllocateVirtualMemory")){
+        number_of_args = 6;
+        index_address = 1;//We need recover_address to get the resulting adress
+        index_protect = 5;
+    } else if (!strcmp(info->trap->name, "NtMapViewOfSection")){
+        number_of_args = 10;
+        index_address = 2;//We need recover_address to get the resulting adress
+        index_protect = 9;
+    } else {
+        printf("Error in recover_address_cb: Do not know the trapped syscall\n");
+        goto exit;
+    }
+
+    ctx.addr = info->regs->rsp + reg_size;  // jump over base pointer
+
+    if ( size != vmi_read(vmi, &ctx, buf, size) )
+                goto exit;
+
+    ctx.addr = buf32[index_address];
+    if(VMI_SUCCESS != vmi_read_32(vmi, &ctx, &buf32[index_address])){
+        printf("ERROR\n");
+    }
+
+    printf("Allocated Address: 0x%" PRIx32 "\n", buf32[index_address]);
+
+exit:
+    g_free(buf);
+    drakvuf_release_vmi(drakvuf);
+  
+    return 0;
+}
+
+int recover_address(drakvuf_t drakvuf, drakvuf_trap_info_t *info, vmi_instance_t *vmi){
+    //To get the address we need to set a breakpoint at the return adress so we get the result address of the allocation
+    packeranalyser *p = (packeranalyser*)info->trap->data;
+    uint32_t return_address = 0;
+    access_context_t ctx;
+    ctx.translate_mechanism = VMI_TM_PROCESS_DTB;
+    ctx.dtb = info->regs->cr3;
+    ctx.addr = info->regs->rsp;
+
+
+    vmi_read_32(*vmi, &ctx, &return_address);
+
+
+    p->get_address_trap.breakpoint.lookup_type = LOOKUP_PID;
+    p->get_address_trap.breakpoint.pid = p->pid;
+    p->get_address_trap.breakpoint.addr_type = ADDR_VA;
+    p->get_address_trap.breakpoint.module = "Win32Project1_packed.exe";
+    p->get_address_trap.name = info->trap->name;
+    p->get_address_trap.type = BREAKPOINT;
+    p->get_address_trap.cb = recover_address_cb;
+    p->get_address_trap.data = p;
+
+    printf("Return_Adress: 0x%" PRIx32 "\n", return_address);
+
+    p->get_address_trap.breakpoint.rva = (addr_t)return_address;//TODO: add return address;
+
+    if ( !drakvuf_add_trap(drakvuf, &p->get_address_trap) ){
+            printf("Couldn't add trap\n");; 
+    }
+
+    return 0;
 }
 
 
@@ -24,18 +113,26 @@ static event_response_t syscall_cb(drakvuf_t drakvuf, drakvuf_trap_info_t *info)
     uint64_t *buf64 = NULL;
 
 
-    printf("%s\n", info->trap->name);
-
+    //TODO: get the size argument for index_address = 0 functions so we can calculate the memory region
     if(!strcmp(info->trap->name, "NtProtectVirtualMemory")){
         number_of_args = 5;
         index_address = 1;
         index_protect = 3;
+    } else if (!strcmp(info->trap->name, "NtAllocateVirtualMemory")){
+        number_of_args = 6;
+        index_address = 0;//We need recover_address to get the resulting adress
+        index_protect = 5;
+    } else if (!strcmp(info->trap->name, "NtMapViewOfSection")){
+        number_of_args = 10;
+        index_address = 0;//We need recover_address to get the resulting adress
+        index_protect = 9;
     } else {
-        recover_address(drakvuf, info);
+        printf("Error in syscall_cb: Do not know the trapped syscall\n");
         goto exit;
     }
 
     
+
     if(p->pid != (int) vmi_dtb_to_pid (vmi, info->regs->cr3)){
         drakvuf_release_vmi(drakvuf);
         return 0;
@@ -60,8 +157,10 @@ static event_response_t syscall_cb(drakvuf_t drakvuf, drakvuf_trap_info_t *info)
             // multiply num args by 4 for 32 bit systems to get the number of bytes we need
             // to read from the stack.  assumes standard calling convention (cdecl) for the
             // visual studio compile.
-            if ( size != vmi_read(vmi, &ctx, buf, size) )
+            if ( size != vmi_read(vmi, &ctx, buf, size) ){
+                printf("Error 1\n");
                 goto exit;
+            }
     }
 
     if ( 8 == reg_size ){
@@ -72,13 +171,21 @@ static event_response_t syscall_cb(drakvuf_t drakvuf, drakvuf_trap_info_t *info)
                 goto exit;
     }
 
-    
-        //printf(" Arguments: %" PRIu32 "\n", 6);
 
+    if(index_address==0){
+        printf("recover_address!\n");
+        recover_address(drakvuf, info, &vmi);
+        goto exit;
+    }
 
 
     if ( 4 == reg_size ){
         if ((buf32[index_protect]&0xF0)==0){//test if executable right is requested if not nothing to do here
+            goto exit;
+        }
+        if(index_address==0){
+            printf("recover_address!\n");
+            recover_address(drakvuf, info, &vmi);
             goto exit;
         }
 
@@ -90,7 +197,7 @@ static event_response_t syscall_cb(drakvuf_t drakvuf, drakvuf_trap_info_t *info)
     }else{
         //printf("0x%" PRIx64, buf64[i]);
     }
-    printf("Callback: Adress: 0x%" PRIx32 " Protect: 0x%" PRIx32 "\n", buf32[index_address], buf32[index_protect]);
+    //printf("Callback: Adress: 0x%" PRIx32 " Protect: 0x%" PRIx32 "\n", buf32[index_address], buf32[index_protect]);
 
     exit:
         g_free(buf);
@@ -175,9 +282,14 @@ packeranalyser::packeranalyser(drakvuf_t drakvuf, const void *config_p, output_f
 
 	if (this->pm == VMI_PM_IA32E){
 		printf("VMI_PM_IA32E\n");
-	}
+    }
 
-        this->ntcontinuecb_trap.breakpoint.lookup_type = LOOKUP_PID;
+    if(vmi_get_address_width(vmi)==8){
+        printf("64 bit not yet supporter\n");
+        throw -1;
+    }
+
+    this->ntcontinuecb_trap.breakpoint.lookup_type = LOOKUP_PID;
     this->ntcontinuecb_trap.breakpoint.pid = 4;
     this->ntcontinuecb_trap.breakpoint.addr_type = ADDR_RVA;
     this->ntcontinuecb_trap.breakpoint.module = "ntoskrnl.exe";
